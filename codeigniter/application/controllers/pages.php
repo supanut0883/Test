5151244c2316f0e5262bf2a19d9f2e32cb4c7d22
supanut <supanut0883@gmail.com>
2017-05-04 09:07:44 +0700

    Test
    RCC

--- codeigniter/application/controllers/pages.php
+++ codeigniter/application/controllers/pages.php
@@ -5,10 +5,6 @@ class Pages extends CI_Controller {
 
 	public function view($name = 'sPk') {
 		$page = 'home';
-
-		// if(!file_exists('application/views/pages/'. $page . '.php')) {
-		// 	show_404();
-		// }
  
 		$data['title'] = ucfirst($page);
 		$data['name'] = $name;
